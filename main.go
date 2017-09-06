dbd1f01de559b362e2b76cffee987c333efbef3b
Alberto Martinez de Murga Ramirez <alberto@threkk.com>
2017-09-06 09:00:51 +0200

    WIP

--- main.go
+++ main.go
@@ -7,9 +7,10 @@ import (
 	"strings"
 )
 
-const VERSION string = "0.0.1"
+const version string = "0.0.1"
+const width = 80
 
-var DOGS = []string{
+var dogs = []string{
 
 	0: ` \ ______/ U'-,
   }        /~~
@@ -32,8 +33,47 @@ _   /      |
  (___/-(____)`,
 }
 
-func printText(text string) {
+func splitInLines(s string) []string {
+	lines := make([]string, 0)
+	words := strings.Split(s, " ")
 
+	acc := 0
+	index := 0
+	for i, word := range words {
+		acc = acc + len(word) + 1 // Separation between words needs to be added.
+		if acc > (width - 4) {
+			line := strings.Join(words[index:i-1], " ")
+			lines = append(lines, line)
+			index = i
+			acc = 0
+		}
+	}
+	return lines
+}
+
+func showBalloon(lines []string) {
+	if len(lines) == 1 && len(lines[0]) < 80 {
+		// Line is smaller tha 80, we need to wrap it.
+		//  _______
+		// < Mong! >
+		//  -------
+	}
+
+}
+
+func showDog(dogType int) {
+	fmt.Println(dogs[dogType])
+}
+
+func showVersion() {
+	fmt.Println(version)
+	os.Exit(0)
+}
+
+func showError(option int) {
+	fmt.Printf("Invalid option: %d.\n\n", option)
+	flag.Usage()
+	os.Exit(1)
 }
 
 func main() {
@@ -42,7 +82,7 @@ func main() {
 
 	// Change the command line
 	flag.Usage = func() {
-		fmt.Printf("Mong says - A dog version of cowsay (v%s)\n", VERSION)
+		fmt.Printf("Mong says - A dog version of cowsay (v%s)\n", version)
 		fmt.Println()
 		fmt.Println("Usage: mongsay [options] <text>")
 		fmt.Println("Options:")
@@ -50,27 +90,23 @@ func main() {
 		flag.PrintDefaults()
 	}
 
-	var isMute *bool = flag.Bool("mute", false, "Print the dog without the speech bubble.")
-	var isVersion *bool = flag.Bool("version", false, "Print the version number.")
-	var dogType *int = flag.Int("type", 0, "Dog version to use: 0 (default), 1 or 2.")
+	isMute := flag.Bool("mute", false, "Print the dog without the speech bubble.")
+	isVersion := flag.Bool("version", false, "Print the version number.")
+	dogType := flag.Int("type", 0, "Dog version to use: 0 (default), 1 or 2.")
 	flag.Parse()
 
 	text := strings.Join(flag.Args(), " ")
 
 	if *isVersion {
-		fmt.Println(VERSION)
-		os.Exit(0)
+		showVersion()
+	}
+
+	if *dogType >= len(dogs) || *dogType < 0 {
+		showError(*dogType)
 	}
 
 	if !*isMute {
-		printText(text)
+		showBalloon(splitInLines(text))
 	}
-
-	if *dogType >= len(DOGS) || *dogType < 0 {
-		fmt.Printf("Invalid option: %d.\n\n", *dogType)
-		flag.Usage()
-		os.Exit(1)
-	} else {
-		fmt.Println(DOGS[*dogType])
-	}
+	showDog(*dogType)
 }
